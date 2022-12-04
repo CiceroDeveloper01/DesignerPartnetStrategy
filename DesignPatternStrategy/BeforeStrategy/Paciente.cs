0b2f4d30312ceaef8d533c1cc469dba8ea0ad94f
cicero.junior.developer <cicero.junior.developer@gmail.com>
2022-12-03 22:11:53 -0300

    Criação da Classe Paciente Deficitária

---
+++ DesignPatternStrategy/BeforeStrategy/Paciente.cs
@@ -0,0 +1,28 @@
+﻿namespace DesignPatternStrategy.BeforeStrategy
+{
+    public class Paciente
+    {
+        public string Nome { get; set; }
+        public float Peso { get; set; }
+        public float Altura { get; set; }
+        public float CalcularImc()
+        {
+            return Peso / (Altura * Altura);
+        }
+        public void PrescreverDieta(string DescricaoDieta)
+        {
+            Console.WriteLine($"{DescricaoDieta} para o Paciente: {Nome} realizada com sucesso.");
+            Console.ReadLine();
+        }
+        public void PrescreverTreinamento(string DescricaoTreinamento)
+        {
+            Console.WriteLine($"{DescricaoTreinamento} para o Paciente: {Nome} realizada com sucesso.");
+            Console.ReadLine();
+        }
+        public void EncaminharParaCirurgiaBariatrica()
+        {
+            Console.WriteLine($"Paciente: {Nome} encaminhado com sucesso para cirurgia báriatrica.");
+            Console.ReadLine();
+        }
+    }
+}
--- DesignPatternStrategy/Program.cs
+++ DesignPatternStrategy/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DesignPatternStrategy.BeforeStrategy;
+
+public static class Program
+{
+
+    static void Main()
+    {
+        Inicio:
+        Console.WriteLine("Para o Etendimento Fazer o Teste Antes do Strategy Aperte 1");
+        Console.WriteLine("Para o Etendimento Fazer o Teste Pós Implementação do Strategy Aperte 2");
+        var opcao = Console.ReadLine();
+        if (int.Parse(opcao) == 1)
+            BeforeStrategy();
+        else if (int.Parse(opcao) == 2)
+            AfterTheStrategy();
+        else
+        {
+            Console.WriteLine("Opção Inválida");
+            Console.ReadLine();
+            Console.Clear();
+            goto Inicio;
+        }
+    }
+
+    static void BeforeStrategy()
+    {
+        Paciente paciente = new Paciente();
+
+        Console.WriteLine("Por Favor, Informe o Nome do Paciente");
+        paciente.Nome = Console.ReadLine();
+
+        Console.WriteLine("Por Favor, Informe a Altura do Paciente");
+        paciente.Altura = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Por Favor, Informe o Peso do Paciente");
+        paciente.Peso = float.Parse(Console.ReadLine());
+
+        var imc = paciente.CalcularImc();
+        Console.WriteLine($"O I.M.C do Paciente {paciente.Nome} é de {imc}");
+
+        OpcaoPaciente:
+        Console.WriteLine($"Deseja fazer o que com o Paciente {paciente.Nome} 1 - Prescerever Dieta; 2 - Prescrever Treinamento; 3 - Encaminhar Para Cirurgia Bariátrica");
+        var opcao = Console.ReadLine();
+
+        if (int.Parse(opcao) == 1)
+        {
+            Console.WriteLine("Informe a Dieta do Cliente");
+            var dieta = Console.ReadLine();
+            paciente.PrescreverDieta(dieta);
+        }
+        else if (int.Parse(opcao) == 2)
+        {
+            Console.WriteLine("Informe o Treino do Cliente");
+            var treinamento = Console.ReadLine();
+            paciente.PrescreverTreinamento(treinamento);
+        }
+        else if (int.Parse(opcao) == 3)
+            paciente.EncaminharParaCirurgiaBariatrica();
+        else
+        {
+            Console.WriteLine("Opção Inválida");
+            Console.ReadLine();
+            goto OpcaoPaciente;
+        }
+    }
+
+    static void AfterTheStrategy()
+    {
+
+    }
+
+}
+
